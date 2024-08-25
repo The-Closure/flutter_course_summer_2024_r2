2861c87dae4c4136da579b900fd1154ce37bf9e7
AbdTll <abdalaziztll992010@gmail.com>
2024-08-25 07:29:15 -0700

    test of git session

---
+++ android/app/src/main/kotlin/com/example/github_session/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.github_session
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+  const  MaterialApp(
+      home: Scaffold(),
+    ),
+  );
+}
