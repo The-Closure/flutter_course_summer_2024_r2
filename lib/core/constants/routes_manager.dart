5361e6c2d6ea4837305d529103f959e221fe5873
Mohammad Alhajjar <144520391+MohammadAlhajjar@users.noreply.github.com>
2024-08-29 18:53:54 +0300

    Delete lib directory

--- lib/core/constants/routes_manager.dart
+++
@@ -1,9 +0,0 @@
-class RoutesManager {
-  static const String splash = '/splash';
-  static const String onboarding = '/onboarding';
-  static const String login1 = '/login1';
-  static const String login2 = '/login2';
-  static const String login3 = '/login3';
-  static const String home = '/home';
-  static const String book = '/book';
-}
